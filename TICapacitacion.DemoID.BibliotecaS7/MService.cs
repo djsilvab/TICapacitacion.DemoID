e315ffc328661b707a266dd8df2d95837b733940
David Silva Bazan <djsilvab@gmail.com>
2025-04-04 22:09:01 -0500

    utilizacion de ServiceProvider

---
+++ TICapacitacion.DemoID.BibliotecaS7/MService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TICapacitacion.DemoID.BibliotecaS7;
+public class MService(ILogger<MService> logger,
+                      IConfiguration configuration) : IDisposable
+{
+    public void LogMessage(string message) 
+        => logger.LogInformation(message);
+
+    public void LogConfigKeyValue(string key)
+        => logger.LogInformation("MService.LogConfigKeyValue: {value}", configuration[key]);
+
+    public void Dispose()
+    {
+        logger.LogInformation("MService Disposed");
+    }
+}
---
+++ TICapacitacion.DemoID.ConsolaAppS7/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TICapacitacion.DemoID.BibliotecaS7;
+
+RunWithoutApplicationHost();
+//RunWithApplicationHost();
+
+void RunWithApplicationHost()
+{
+    Console.WriteLine("With Application Host Example");
+    IHostBuilder Builder = Host.CreateDefaultBuilder();
+    Builder.ConfigureServices(services =>
+    {
+        services.AddSingleton<MService>();
+    });
+    using IHost AppHost = Builder.Build();
+    MHelperService.DoSomeWork(AppHost.Services);
+}
+
+void RunWithoutApplicationHost()
+{
+    Console.WriteLine("Without Application Host Example");
+    IServiceCollection Services = new ServiceCollection();
+    
+    Services.AddLogging(config => 
+    {
+        config.AddConsole();
+        config.AddDebug();
+    });
+
+    IConfiguration Config = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+
+    Services.AddSingleton(Config);
+    Services.AddSingleton<MService>();
+
+    ServiceProvider Provider = Services.BuildServiceProvider();
+
+    MHelperService.DoSomeWork(Provider);
+
+    Provider.Dispose();
+}
