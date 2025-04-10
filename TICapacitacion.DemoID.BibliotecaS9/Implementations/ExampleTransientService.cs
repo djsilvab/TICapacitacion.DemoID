99576f57bbef6c52d5e246d3fd388241fd6d6530
David Silva Bazan <djsilvab@gmail.com>
2025-04-10 00:43:41 -0500

    sesion_9

---
+++ TICapacitacion.DemoID.BibliotecaS9/Implementations/ExampleTransientService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TICapacitacion.DemoID.BibliotecaS9.Interfaces;
+
+namespace TICapacitacion.DemoID.BibliotecaS9.Implementations;
+
+public class ExampleTransientService : IExampleTransientService
+{
+    public ExampleTransientService()
+        => (Id, LifeTime) = (Guid.NewGuid(), ServiceLifetime.Transient);
+
+    public Guid Id { get; }
+
+    public ServiceLifetime LifeTime { get; }
+}
---
+++ TICapacitacion.DemoID.BibliotecaS9/Implementations/ServiceLifeTimeReporter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using TICapacitacion.DemoID.BibliotecaS9.Interfaces;
+
+namespace TICapacitacion.DemoID.BibliotecaS9.Implementations;
+
+public class ServiceLifeTimeReporter(
+        IExampleTransientService transientService,
+        IExampleScopedService scopedService,
+        IExampleSingletonService singletonService,
+        ILogger<ServiceLifeTimeReporter> logger
+    ) : IServiceLifeTimeReporter
+{
+    public void ReportServiceLifeTimeDetails(string lifetime, string details)
+    {
+        //BeginScope: permite agrupar los mensajes
+        using var S = logger.BeginScope("{0}: {1}", lifetime, details);
+
+        LogServiceInfo(transientService, "Always different");
+        LogServiceInfo(scopedService, "Changes only with lifetime");
+        LogServiceInfo(singletonService, "Always the same");
+    }
+
+    void LogServiceInfo<T>(T service, string message)
+        where T : IReportServiceLifeTime
+        => logger.LogInformation("\t {type}: {id}\n\t ({message})", typeof(T).Name, service.Id, message);
+
+}
